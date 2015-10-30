84865288498e86fc819ab07457242eae35389bd8
Gabi Gutu <gabi.gutu@gmail.com>
2015-10-30 15:58:15 +0200

    SentimentEntity improvement

--- src/main/java/DAO/sentiment/SentimentEntity.java
+++ src/main/java/DAO/sentiment/SentimentEntity.java
@@ -1,14 +1,12 @@
 package DAO.sentiment;
 
 import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.Map;
 
 /**
  * Holds a HashMap containing different sentiments and their 
- * quantifiers.
- * The available sentiments are: very negative, negative, 
- * neutral, positive and very positive.
- * Each sentiment has a value representing it's relevance.
+ * scores.
+ * Sentiments are stored as valences from the Valences class.
  *   
  * @author Gabriel Gutu
  *
@@ -16,78 +14,99 @@ import java.util.Map.Entry;
 public class SentimentEntity {
 	
 	/**
-	 * Constants representing the sentiments that can be held. 
+	 * HashMap storing the valences and their quantifier 
 	 */
-	public static final int VERY_NEGATIVE 	= -2;
-	public static final int NEGATIVE 		= -1;
-	public static final int NEUTRAL 		= 0;
-	public static final int POSITIVE 		= 1;
-	public static final int VERY_POSITIVE 	= 2;
-
-	/**
-	 * HashMap storing the quantifiers for each sentiment. 
-	 */
-	private HashMap<Integer, Double> sentiments;
+	private Map<Integer, Double> sentiments;
 	
 	/**
-	 * HashMap storing the text representation for each sentiment.
-	 */
-	private HashMap<Integer, String> sentimentsToString;
-	
-	/**
-	 * Initializes an empty HashMap for the sentiments and for the 
-	 * sentimentsToString representation and builds the sentimentsToString
-	 * HashMap values
+	 * Initializes an empty Map for the sentiments 
 	 */
 	public SentimentEntity() {
 		sentiments = new HashMap<>();
-		sentimentsToString = new HashMap<>();
-		
-		// TODO: replace strings with values from .properties file
-		sentimentsToString.put(VERY_NEGATIVE, 	"Very Negative");
-		sentimentsToString.put(NEGATIVE, 		"Negative");
-		sentimentsToString.put(NEUTRAL, 		"Neutral");
-		sentimentsToString.put(POSITIVE, 		"Positive");
-		sentimentsToString.put(VERY_POSITIVE, 	"Very Positive");
 	}
 
 	/**
-	 * Initializes an empty HashMap for the sentiments and for the 
-	 * sentimentsToString representation and builds the sentimentsToString
-	 * HashMap values and adds the sentiment "key" with the score "value"  
+	 * Adds a sentiment with the valence "key" and 
+	 * the score "value" to the sentiment Map
 	 * 
 	 * @param key
-	 * 			the sentiment to be added (should be used as constant) 
-	 * @param value
-	 * 			sentiment's quantifier
-	 */
-	public SentimentEntity(Integer key, double value) {
-		this();
-		add(key, value);
-	}
-
-	/**
-	 * Adds the sentiment "key" with the score "value" to the
-	 * sentiment HashMap
-	 * 
-	 * @param key
-	 * 			the sentiment to be added (should be used as constant)
+	 * 			the valence to be added (should be 
+	 * 			used as one constant from the Valences class)
 	 * @param value
 	 * 			sentiment's quantifier
 	 */
 	public void add(Integer key, double value) {
 		sentiments.put(key, value);
 	}
-
+	
 	/**
-	 * @return
-	 * 			sentiments representation
+	 * Removes the sentiment with the valence "key"
+	 * from the sentiment Map
+	 *  
+	 * @param key
+	 * 			the valence of the sentiment that
+	 * 			has to be removed
 	 */
-	public HashMap<Integer, Double> getSentiments() {
-		return sentiments;
+	public void remove(Integer key) {
+		if (sentiments.containsKey(key)) {
+			sentiments.remove(key);
+		}
+	}
+	
+	/**
+	 * Gets the score for the sentiment with the 
+	 * valence "key" and returns the sentiment value
+	 * if sentiment was found. Otherwise, returns null
+	 * 
+	 * @param key
+	 * 			the sentiment to be selected (should be 
+	 * 			used as one constant from the Valences class)
+	 * @return
+	 * 			the score of the sentiment if the sentiment 
+	 * 			was found; otherwise, null 
+	 */
+	public Double get(Integer key) {
+		if (sentiments.containsKey(key)) {
+			return sentiments.get(key);
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Sets the score "value" for the valence "key"
+	 * 
+	 * @param key
+	 * 			the valence to be added (should be used as constant)
+	 * @param value
+	 * 			sentiment's quantifier
+	 */
+	public void set(Integer key, double value) {
+		if (sentiments.containsKey(key)) {
+			sentiments.put(key, value);
+		}
 	}
 
 	/**
+	 * Returns the sentiments Map 
+	 * 
+	 * @return
+	 * 			sentiments representation
+	 */
+	public Map<Integer, Double> getAll() {
+		return sentiments;
+	}
+	
+	/**
+	 * Removes all the sentiments
+	 */
+	public void removeAll() {
+		sentiments = null;
+	}
+
+	/**
+	 * Returns the size of the sentiments Map 
+	 * 
 	 * @return
 	 * 			number of held sentiments
 	 */
@@ -95,92 +114,24 @@ public class SentimentEntity {
 		return sentiments.size();
 	}
 
-	/**
-	 * Sets the score "value" for the sentiment "key" and 
-	 * returns the key if sentiment was found. Otherwise, 
-	 * returns -1
-	 * 
-	 * @param key
-	 * 			the sentiment to be added (should be used as constant)
-	 * @param value
-	 * 			sentiment's quantifier
-	 * @return
-	 * 			the key if sentiment was found; -1 otherwise
-	 */
-	public Integer setValue(Integer key, double value) {
-		if (sentiments.containsKey(key)) {
-			sentiments.put(key, value);
-			return key;
-		} else {
-			return -1;
-		}
-	}
-
-	/**
-	 * Gets the score for the sentiment "key" and returns
-	 * the key if sentiment was found. Otherwise, returns -1.0
-	 * 
-	 * @param key
-	 * 			the sentiment to be selected (should be used as constant)
-	 * @return
-	 * 			the score of the sentiment it sentiment was found; 
-	 * 			otherwise, -1.0 
-	 */
-	public Double getValue(Integer key) {
-		if (sentiments.containsKey(key)) {
-			return sentiments.get(key);
-		} else {
-			return -1.0;
-		}
-	}
-
-	/**
-	 * Gets current sentiment using an iterator.
-	 * 
-	 * @return
-	 * 			current sentiment if available; otherwise, null
-	 */
-	public Entry<Integer, Double> get() {
-		if (!sentiments.isEmpty()) {
-			return sentiments.entrySet().iterator().next();
-		} else {
-			return null;
-		}
-	}
-	
-	/**
-	 * Gets current sentiment's value using an iterator. 
-	 * 
-	 * @return
-	 * 			current sentiment's value if available; otherwise, null
-	 */
-	public Double getValue() {
-		return getValue(sentiments.entrySet().iterator().next().getKey());
-	}
-
-	/**
-	 * Gets current sentiment's key using an iterator. 
-	 * 
-	 * @return
-	 * 			current sentiment's key if available; otherwise, null
-	 */
-	public Integer getKey() {
-		if (!sentiments.isEmpty()) {
-			return sentiments.entrySet().iterator().next().getKey();
-		} else {
-			return null;
-		}
-	}
-
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("[");
 		for (Integer key : sentiments.keySet()) {
-			sb.append(sentimentsToString.get(key));
+			try {
+				sb.append(Valences.getValenceName(key).toString());
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			sb.append("=");
 			sb.append(sentiments.get(key));
 			sb.append(",");
 		}
+		// delete trailing comma
 		sb.deleteCharAt(sb.length()-1);
 		sb.append("]");
 		return sb.toString();
