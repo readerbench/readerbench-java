f5072d2e6704a01534622612dffa296abfc34d3c
Gabi Gutu <gabi.gutu@gmail.com>
2016-01-18 12:29:36 +0200

    Minor bugs

--- src/main/java/webService/services/TextualComplexity.java
+++ src/main/java/webService/services/TextualComplexity.java
@@ -7,8 +7,7 @@ import data.AbstractDocument;
 import services.commons.Formatting;
 import services.complexity.ComplexityIndices;
 import services.complexity.IComplexityFactors;
-import webService.result.ResultSentiment;
-import webService.result.ResultValence;
+import webService.result.*;
 
 public class TextualComplexity {
 	
@@ -29,7 +28,7 @@ public class TextualComplexity {
 				localResults.add(new ResultValence(ComplexityIndices.TEXTUAL_COMPLEXITY_INDEX_ACRONYMS[id],
 						Formatting.formatNumber(queryDoc.getComplexityIndices()[id])));
 			}
-			resultsComplexity.add(new ResultSentiment(complexityClass.getClassName(), localResults));
+			resultsComplexity.add(new ResultSentiment(complexityClass.getClassName(), localResults, null));
 		}
 
 		return resultsComplexity;
