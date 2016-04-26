e5bff037e5bc2af536fd7f808786d58d2000b1ad
Mihai Dascalu <mikedascalu@yahoo.com>
2016-04-26 17:43:47 +0300

    Bug fix Connectives (FR, RO)

--- src/main/java/services/complexity/discourse/ConnectivesComplexity.java
+++ src/main/java/services/complexity/discourse/ConnectivesComplexity.java
@@ -12,131 +12,131 @@ import utils.localization.LocalizationUtils;
 
 public class ConnectivesComplexity extends IComplexityFactors {
 
-    private Lang lang;
+	private Lang lang;
 
-    public ConnectivesComplexity(Lang lang) {
-        this.lang = lang;
-    }
+	public ConnectivesComplexity(Lang lang) {
+		this.lang = lang;
+	}
 
-    public String getClassName() {
-        return LocalizationUtils.getTranslation("Discourse Factors (Connectives)") + " (" + lang + ")";
-    }
+	public String getClassName() {
+		return LocalizationUtils.getTranslation("Discourse Factors (Connectives)") + " (" + lang + ")";
+	}
 
-    public void setComplexityIndexDescription(String[] descriptions) {
-        int index = 0;
-        switch (lang) {
-            case eng:
-                for (String className : Connectives.CONNECTIVES_EN.getClasses().keySet()) {
-                    descriptions[ComplexityIndices.CONNECTIVES_EN + index] = "Average number of "
-                            + className.replaceAll("_", " ") + " connectives per block (EN)";
-                    index++;
-                }
-                break;
-            case fr:
-                for (String className : Connectives.CONNECTIVES_FR.getClasses().keySet()) {
-                    descriptions[ComplexityIndices.CONNECTIVES_FR + index] = "Nombre moyen de connecteur "
-                            + className.replaceAll("_", " ") + " selon un bloc (FR)";
-                    index++;
-                }
-                break;
-            case ro:
-                for (String className : Connectives.CONNECTIVES_RO.getClasses().keySet()) {
-                    descriptions[ComplexityIndices.CONNECTIVES_RO + index] = "Număr mediu de conectori"
-                            + className.replaceAll("_", " ") + " conectori per paragraf (RO)";
-                    index++;
-                }
-            default:
-                break;
-        }
-    }
+	public void setComplexityIndexDescription(String[] descriptions) {
+		int index = 0;
+		switch (lang) {
+		case eng:
+			for (String className : Connectives.CONNECTIVES_EN.getClasses().keySet()) {
+				descriptions[ComplexityIndices.CONNECTIVES_EN + index] = "Average number of "
+						+ className.replaceAll("_", " ") + " connectives per block (EN)";
+				index++;
+			}
+			break;
+		case fr:
+			for (String className : Connectives.CONNECTIVES_FR.getClasses().keySet()) {
+				descriptions[ComplexityIndices.CONNECTIVES_FR + index] = "Nombre moyen de connecteur "
+						+ className.replaceAll("_", " ") + " selon un bloc (FR)";
+				index++;
+			}
+			break;
+		case ro:
+			for (String className : Connectives.CONNECTIVES_RO.getClasses().keySet()) {
+				descriptions[ComplexityIndices.CONNECTIVES_RO + index] = "Număr mediu de conectori"
+						+ className.replaceAll("_", " ") + " conectori per paragraf (RO)";
+				index++;
+			}
+		default:
+			break;
+		}
+	}
 
-    public void setComplexityIndexAcronym(String[] acronyms) {
-        int index = 0;
-        switch (lang) {
-            case eng:
-                for (String className : Connectives.CONNECTIVES_EN.getClasses().keySet()) {
-                    String classNameAcronym = WordUtils.capitalizeFully(className, new char[]{'_'}).replaceAll("_", "");
-                    acronyms[ComplexityIndices.CONNECTIVES_EN + index] = "Avg" + classNameAcronym + "BlEn";
-                    index++;
-                }
-                break;
-            case fr:
-                for (String className : Connectives.CONNECTIVES_FR.getClasses().keySet()) {
-                    String classNameAcronym = WordUtils.capitalizeFully(className, new char[]{'_'}).replaceAll("_", "");
-                    acronyms[ComplexityIndices.CONNECTIVES_FR + index] = "Avg" + classNameAcronym + "BlFr";
-                    index++;
-                }
-                break;
-            case ro:
-                for (String className : Connectives.CONNECTIVES_RO.getClasses().keySet()) {
-                    String classNameAcronym = WordUtils.capitalizeFully(className, new char[]{'_'}).replaceAll("_", "");
-                    acronyms[ComplexityIndices.CONNECTIVES_RO + index] = "Avg" + classNameAcronym + "BlRo";
-                    index++;
-                }
-            default:
-                break;
-        }
-    }
+	public void setComplexityIndexAcronym(String[] acronyms) {
+		int index = 0;
+		switch (lang) {
+		case eng:
+			for (String className : Connectives.CONNECTIVES_EN.getClasses().keySet()) {
+				String classNameAcronym = WordUtils.capitalizeFully(className, new char[] { '_' }).replaceAll("_", "");
+				acronyms[ComplexityIndices.CONNECTIVES_EN + index] = "Avg" + classNameAcronym + "BlEn";
+				index++;
+			}
+			break;
+		case fr:
+			for (String className : Connectives.CONNECTIVES_FR.getClasses().keySet()) {
+				String classNameAcronym = WordUtils.capitalizeFully(className, new char[] { '_' }).replaceAll("_", "");
+				acronyms[ComplexityIndices.CONNECTIVES_FR + index] = "Avg" + classNameAcronym + "BlFr";
+				index++;
+			}
+			break;
+		case ro:
+			for (String className : Connectives.CONNECTIVES_RO.getClasses().keySet()) {
+				String classNameAcronym = WordUtils.capitalizeFully(className, new char[] { '_' }).replaceAll("_", "");
+				acronyms[ComplexityIndices.CONNECTIVES_RO + index] = "Avg" + classNameAcronym + "BlRo";
+				index++;
+			}
+		default:
+			break;
+		}
+	}
 
-    public int[] getIDs() {
-        int[] ids = null;
-        System.out.println("Language is " + lang);
-        switch (lang) {
-            case eng:
-                ids = new int[Connectives.NO_CONNECTIVE_TYPES_EN];
-                for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_EN; i++) {
-                    ids[i] = ComplexityIndices.CONNECTIVES_EN + i;
-                }
-                break;
-            case fr:
-                ids = new int[Connectives.NO_CONNECTIVE_TYPES_FR];
-                for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_FR; i++) {
-                    ids[i] = ComplexityIndices.CONNECTIVES_EN + i;
-                }
-                break;
-            case ro:
-                ids = new int[Connectives.NO_CONNECTIVE_TYPES_RO];
-                for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_RO; i++) {
-                    ids[i] = ComplexityIndices.CONNECTIVES_EN + i;
-                }
-                break;
-            default:
-                break;
-        }
-        return ids;
-    }
+	public int[] getIDs() {
+		int[] ids = null;
+		System.out.println("Language is " + lang);
+		switch (lang) {
+		case eng:
+			ids = new int[Connectives.NO_CONNECTIVE_TYPES_EN];
+			for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_EN; i++) {
+				ids[i] = ComplexityIndices.CONNECTIVES_EN + i;
+			}
+			break;
+		case fr:
+			ids = new int[Connectives.NO_CONNECTIVE_TYPES_FR];
+			for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_FR; i++) {
+				ids[i] = ComplexityIndices.CONNECTIVES_FR + i;
+			}
+			break;
+		case ro:
+			ids = new int[Connectives.NO_CONNECTIVE_TYPES_RO];
+			for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_RO; i++) {
+				ids[i] = ComplexityIndices.CONNECTIVES_RO + i;
+			}
+			break;
+		default:
+			break;
+		}
+		return ids;
+	}
 
-    public void computeComplexityFactors(AbstractDocument document) {
-        for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_EN + Connectives.NO_CONNECTIVE_TYPES_FR
-                + Connectives.NO_CONNECTIVE_TYPES_RO; i++) {
-            document.getComplexityIndices()[ComplexityIndices.CONNECTIVES_EN + i] = ComplexityIndices.IDENTITY;
-        }
+	public void computeComplexityFactors(AbstractDocument document) {
+		for (int i = 0; i < Connectives.NO_CONNECTIVE_TYPES_EN + Connectives.NO_CONNECTIVE_TYPES_FR
+				+ Connectives.NO_CONNECTIVE_TYPES_RO; i++) {
+			document.getComplexityIndices()[ComplexityIndices.CONNECTIVES_EN + i] = ComplexityIndices.IDENTITY;
+		}
 
-        ClassesOfWords classes = null;
-        int index = 0;
-        switch (document.getLanguage()) {
-            case eng:
-                classes = Connectives.CONNECTIVES_EN;
-                index = ComplexityIndices.CONNECTIVES_EN;
-                break;
-            case fr:
-                classes = Connectives.CONNECTIVES_FR;
-                index = ComplexityIndices.CONNECTIVES_FR;
-                break;
-            case ro:
-                classes = Connectives.CONNECTIVES_RO;
-                index = ComplexityIndices.CONNECTIVES_RO;
-                break;
-            default:
-                classes = null;
-                break;
-        }
+		ClassesOfWords classes = null;
+		int index = 0;
+		switch (document.getLanguage()) {
+		case eng:
+			classes = Connectives.CONNECTIVES_EN;
+			index = ComplexityIndices.CONNECTIVES_EN;
+			break;
+		case fr:
+			classes = Connectives.CONNECTIVES_FR;
+			index = ComplexityIndices.CONNECTIVES_FR;
+			break;
+		case ro:
+			classes = Connectives.CONNECTIVES_RO;
+			index = ComplexityIndices.CONNECTIVES_RO;
+			break;
+		default:
+			classes = null;
+			break;
+		}
 
-        if (classes != null) {
-            for (String className : classes.getClasses().keySet()) {
-                document.getComplexityIndices()[index] = classes.countAveragePatternOccurrences(document, className);
-                index++;
-            }
-        }
-    }
+		if (classes != null) {
+			for (String className : classes.getClasses().keySet()) {
+				document.getComplexityIndices()[index] = classes.countAveragePatternOccurrences(document, className);
+				index++;
+			}
+		}
+	}
 }
