9e0ae329c0273a09be5632189c42aae37f360f9b
Mihai Dascalu <mikedascalu@yahoo.com>
2016-07-27 21:47:15 +0300

    Improvements Dutch textual complexity

--- src/main/java/services/comprehensionModel/ComprehensionModel.java
+++ src/main/java/services/comprehensionModel/ComprehensionModel.java
@@ -20,114 +20,110 @@ import services.comprehensionModel.utils.pageRank.PageRank;
 import services.semanticModels.LDA.LDA;
 
 public class ComprehensionModel {
-	private double minActivationThreshold;
-	private int maxNoActiveWords;
-	private int maxNoActiveWordsIncrement;
-	private ActivationScoreLogger activationScoreLogger;
 
-	private QueryIndexer queryIndexer;
-	public CMGraphDO currentGraph;
+    private final double minActivationThreshold;
+    private final int maxNoActiveWords;
+    private final int maxNoActiveWordsIncrement;
+    private final ActivationScoreLogger activationScoreLogger;
 
-	public ComprehensionModel(String text, int hdpGrade, int noTopSimilarWords, double minActivationThreshold,
-			int maxNoActiveWords, int maxNoActiveWordsIncrement) {
+    private final QueryIndexer queryIndexer;
+    public CMGraphDO currentGraph;
 
-		this.queryIndexer = new QueryIndexer(text, LDA.loadLDA("resources/in/HDP/grade" + hdpGrade, Lang.eng),
-				noTopSimilarWords);
-		this.currentGraph = new CMGraphDO();
-		this.minActivationThreshold = minActivationThreshold;
-		this.maxNoActiveWords = maxNoActiveWords;
-		this.maxNoActiveWordsIncrement = maxNoActiveWordsIncrement;
-		this.activationScoreLogger = new ActivationScoreLogger();
-	}
+    public ComprehensionModel(String text, int hdpGrade, int noTopSimilarWords, double minActivationThreshold,
+            int maxNoActiveWords, int maxNoActiveWordsIncrement) {
+        this.queryIndexer = new QueryIndexer(text, LDA.loadLDA("resources/in/HDP/grade" + hdpGrade, Lang.eng),
+                noTopSimilarWords);
+        this.currentGraph = new CMGraphDO();
+        this.minActivationThreshold = minActivationThreshold;
+        this.maxNoActiveWords = maxNoActiveWords;
+        this.maxNoActiveWordsIncrement = maxNoActiveWordsIncrement;
+        this.activationScoreLogger = new ActivationScoreLogger();
+    }
 
-	public WordDistanceIndexer getSemanticIndexer() {
-		return this.queryIndexer.getSemanticIndexer();
-	}
+    public WordDistanceIndexer getSemanticIndexer() {
+        return this.queryIndexer.getSemanticIndexer();
+    }
 
-	public int getTotalNoOfPhrases() {
-		return this.queryIndexer.getSyntacticIndexerList().size();
-	}
+    public int getTotalNoOfPhrases() {
+        return this.queryIndexer.getSyntacticIndexerList().size();
+    }
 
-	public Sentence getSentenceAtIndex(int index) {
-		return this.queryIndexer.document.getSentencesInDocument().get(index);
-	}
+    public Sentence getSentenceAtIndex(int index) {
+        return this.queryIndexer.document.getSentencesInDocument().get(index);
+    }
 
-	public WordDistanceIndexer getSyntacticIndexerAtIndex(int index) {
-		return this.queryIndexer.getSyntacticIndexerList().get(index);
-	}
+    public WordDistanceIndexer getSyntacticIndexerAtIndex(int index) {
+        return this.queryIndexer.getSyntacticIndexerList().get(index);
+    }
 
-	public Map<CMNodeDO, Double> getNodeActivationScoreMap() {
-		return this.queryIndexer.getNodeActivationScoreMap();
-	}
+    public Map<CMNodeDO, Double> getNodeActivationScoreMap() {
+        return this.queryIndexer.getNodeActivationScoreMap();
+    }
 
-	public void updateActivationScoreMapAtIndex(int index) {
-		WordDistanceIndexer indexer = this.getSyntacticIndexerAtIndex(index);
-		for (int i = 0; i < indexer.wordList.size(); i++) {
-			CMNodeDO node = new CMNodeDO();
-			node.nodeType = CMNodeType.Syntactic;
-			node.word = indexer.wordList.get(i);
-			double score = this.getNodeActivationScoreMap().get(node);
-			score++;
-			this.getNodeActivationScoreMap().put(node, score);
-		}
-		for (CMNodeDO otherNode : this.currentGraph.nodeList) {
-			if (!this.getNodeActivationScoreMap().containsKey(otherNode)) {
-				this.getNodeActivationScoreMap().put(otherNode, 0.0);
-			}
-		}
-	}
+    public void updateActivationScoreMapAtIndex(int index) {
+        WordDistanceIndexer indexer = this.getSyntacticIndexerAtIndex(index);
+        for (int i = 0; i < indexer.wordList.size(); i++) {
+            CMNodeDO node = new CMNodeDO();
+            node.nodeType = CMNodeType.Syntactic;
+            node.word = indexer.wordList.get(i);
+            double score = this.getNodeActivationScoreMap().get(node);
+            score++;
+            this.getNodeActivationScoreMap().put(node, score);
+        }
+        this.currentGraph.nodeList.stream().filter((otherNode) -> (!this.getNodeActivationScoreMap().containsKey(otherNode))).forEach((otherNode) -> {
+            this.getNodeActivationScoreMap().put(otherNode, 0.0);
+        });
+    }
 
-	public void markAllNodesAsInactive() {
-		for (CMNodeDO node : this.currentGraph.nodeList) {
-			node.nodeType = CMNodeType.Inactive;
-		}
-	}
+    public void markAllNodesAsInactive() {
+        this.currentGraph.nodeList.stream().forEach((node) -> {
+            node.nodeType = CMNodeType.Inactive;
+        });
+    }
 
-	public void applyPageRank(int sentenceIndex) {
-		PageRank pageRank = new PageRank();
-		Map<CMNodeDO, Double> updatedNodeActivationScoreMap = pageRank.runPageRank(this.getNodeActivationScoreMap(),
-				this.currentGraph);
+    public void applyPageRank(int sentenceIndex) {
+        PageRank pageRank = new PageRank();
+        Map<CMNodeDO, Double> updatedNodeActivationScoreMap = pageRank.runPageRank(this.getNodeActivationScoreMap(),
+                this.currentGraph);
 
-		int maxWords = this.maxNoActiveWords + (sentenceIndex * this.maxNoActiveWordsIncrement);
+        int maxWords = this.maxNoActiveWords + (sentenceIndex * this.maxNoActiveWordsIncrement);
 
-		List<NodeRank> nodeRankList = NodeRank.convertMapToNodeRankList(updatedNodeActivationScoreMap);
-		Collections.sort(nodeRankList, Collections.reverseOrder());
-		
-		System.out.println(updatedNodeActivationScoreMap);
-		this.activateFirstWords(updatedNodeActivationScoreMap, nodeRankList, maxWords);
-		System.out.println(updatedNodeActivationScoreMap);
-		
-		Iterator<CMNodeDO> nodeIterator = updatedNodeActivationScoreMap.keySet().iterator();
-		while (nodeIterator.hasNext()) {
-			CMNodeDO node = nodeIterator.next();
-			this.getNodeActivationScoreMap().put(node, updatedNodeActivationScoreMap.get(node));
-		}
+        List<NodeRank> nodeRankList = NodeRank.convertMapToNodeRankList(updatedNodeActivationScoreMap);
+        Collections.sort(nodeRankList, Collections.reverseOrder());
 
-		this.activationScoreLogger.saveScores(updatedNodeActivationScoreMap);
-	}
+        this.activateFirstWords(updatedNodeActivationScoreMap, nodeRankList, maxWords);
 
-	private void activateFirstWords(Map<CMNodeDO, Double> updatedNodeActivationScoreMap, List<NodeRank> nodeRankList,
-			int maxWords) {
-		int noActivatedWord = 0;
-		Set<CMNodeDO> activeNodeSet = new TreeSet<CMNodeDO>();
-		for (NodeRank nodeRank : nodeRankList) {
-			if (nodeRank.value < this.minActivationThreshold) {
-				break;
-			}
-			for (CMNodeDO currentNode : this.currentGraph.nodeList) {
-				if (currentNode.equals(nodeRank.node)) {
-					if (currentNode.nodeType == CMNodeType.Inactive) {
-						currentNode.nodeType = CMNodeType.Active;
-					}
-					activeNodeSet.add(currentNode);
-					noActivatedWord++;
-					break;
-				}
-			}
-			if (noActivatedWord >= maxWords) {
-				break;
-			}
-		}
+        Iterator<CMNodeDO> nodeIterator = updatedNodeActivationScoreMap.keySet().iterator();
+        while (nodeIterator.hasNext()) {
+            CMNodeDO node = nodeIterator.next();
+            this.getNodeActivationScoreMap().put(node, updatedNodeActivationScoreMap.get(node));
+        }
+
+        this.activationScoreLogger.saveScores(updatedNodeActivationScoreMap);
+    }
+
+    private void activateFirstWords(Map<CMNodeDO, Double> updatedNodeActivationScoreMap, List<NodeRank> nodeRankList,
+            int maxWords) {
+        int noActivatedWord = 0;
+        Set<CMNodeDO> activeNodeSet = new TreeSet<>();
+        for (NodeRank nodeRank : nodeRankList) {
+            if (nodeRank.value < this.minActivationThreshold) {
+                break;
+            }
+            for (CMNodeDO currentNode : this.currentGraph.nodeList) {
+                if (currentNode.equals(nodeRank.node)) {
+                    if (currentNode.nodeType == CMNodeType.Inactive) {
+                        currentNode.nodeType = CMNodeType.Active;
+                    }
+                    activeNodeSet.add(currentNode);
+                    noActivatedWord++;
+                    break;
+                }
+            }
+            if (noActivatedWord >= maxWords) {
+                break;
+            }
+        }
 //		updatedNodeActivationScoreMap.keySet().forEach((node) -> {
 //			if (!activeNodeSet.contains(node)) {
 //				double oldValue = updatedNodeActivationScoreMap.get(node);
@@ -136,11 +132,11 @@ public class ComprehensionModel {
 //				updatedNodeActivationScoreMap.put(node, 0.0);
 //			}
 //		});
-	}
+    }
 
-	public void logSavedScores(CMGraphDO syntacticGraph) {
-		this.activationScoreLogger.saveNodes(syntacticGraph);
-		this.activationScoreLogger.saveNodes(this.currentGraph);
-		this.activationScoreLogger.logSavedScores();
-	}
+    public void logSavedScores(CMGraphDO syntacticGraph, int sentenceIndex) {
+        this.activationScoreLogger.saveNodes(syntacticGraph);
+        this.activationScoreLogger.saveNodes(this.currentGraph);
+        this.activationScoreLogger.logSavedScores();
+    }
 }
--- src/main/java/services/comprehensionModel/utils/ActivationScoreLogger.java
+++ src/main/java/services/comprehensionModel/utils/ActivationScoreLogger.java
@@ -9,97 +9,94 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import data.Word;
+import org.openide.util.Exceptions;
 import services.comprehensionModel.utils.indexer.graphStruct.CMGraphDO;
 import services.comprehensionModel.utils.indexer.graphStruct.CMNodeDO;
 import services.comprehensionModel.utils.indexer.graphStruct.CMNodeType;
 
 public class ActivationScoreLogger {
-	public static String OUTPUT_FILE_NAME = "out/comprehension_model_scores.csv";
-	private List<Map<Word, Double>> activationHistory;
-	private List<CMNodeDO> uniqueWordList;
-	
-	public ActivationScoreLogger() {
-		this.activationHistory = new ArrayList<Map<Word, Double>>();
-		this.uniqueWordList = new ArrayList<CMNodeDO>();
-	}
-	
-	public void saveScores(Map<CMNodeDO, Double> activationMap) {
-		Map<Word, Double> activationMapCopy = new TreeMap<Word, Double>();
-		Iterator<CMNodeDO> nodeIterator = activationMap.keySet().iterator();
-		while(nodeIterator.hasNext()) {
-			CMNodeDO currentNode = nodeIterator.next();
-			activationMapCopy.put(currentNode.word, activationMap.get(currentNode));
-		}
-		this.activationHistory.add(activationMapCopy);
-	}
-	public void saveNodes(CMGraphDO graph) {
-		for(CMNodeDO node : graph.nodeList) {
-			this.addNodeIfNotExists(node);
-		}
-	}
-	private void addNodeIfNotExists(CMNodeDO nodeToAdd) {
-		boolean exists = false;
-		for(int i = 0; i < this.uniqueWordList.size(); i ++) {
-			CMNodeDO currentNode = this.uniqueWordList.get(i);
-			if(currentNode.equals(nodeToAdd)) {
-				if(currentNode.nodeType != CMNodeType.Syntactic && nodeToAdd.nodeType == CMNodeType.Syntactic) {
-					this.uniqueWordList.set(i, nodeToAdd);
-				}
-				exists = true;
-				break;
-			}
-		}
-		if(!exists) {
-			this.uniqueWordList.add(nodeToAdd);
-		}
-	}
-	
-	public void logSavedScores() {
-		try {
-			FileWriter fwrt = new FileWriter(ActivationScoreLogger.OUTPUT_FILE_NAME);
-			BufferedWriter bfwrt = new BufferedWriter(fwrt);
-			
-			String header = "Word,Type";
-			for(int  i = 0; i < activationHistory.size(); i++) {
-				header += ",Phrase " + (i + 1);
-			}
-			bfwrt.write(header);
-			
-			for(CMNodeDO node : this.uniqueWordList) {
-				if(node.nodeType == CMNodeType.Syntactic) {
-					String line = this.getLogLineForNode(node, "Syntactic");
-					bfwrt.newLine();
-					bfwrt.write(line);
-				}
-			}
-			
-			for(CMNodeDO node : this.uniqueWordList) {
-				if(node.nodeType != CMNodeType.Syntactic) {
-					String line = this.getLogLineForNode(node, "Semantic");
-					bfwrt.newLine();
-					bfwrt.write(line);
-				}
-			}
-			
-			bfwrt.close();
-			fwrt.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private String getLogLineForNode(CMNodeDO node, String nodeType) {
-		String line = node.word.getLemma() + "," + nodeType;
-		
-		for(Map<Word, Double> activationMap : this.activationHistory) {
-			if(!activationMap.containsKey(node.word)) {
-				line += ",0"; 
-			}
-			else {
-				line += "," + activationMap.get(node.word);
-			}
-		}
-		return line;
-	}
-	
+
+    public static final String OUTPUT_FILE_NAME = "out/comprehension_model_scores.csv";
+    private final List<Map<Word, Double>> activationHistory;
+    private final List<CMNodeDO> uniqueWordList;
+
+    public ActivationScoreLogger() {
+        this.activationHistory = new ArrayList<>();
+        this.uniqueWordList = new ArrayList<>();
+    }
+
+    public void saveScores(Map<CMNodeDO, Double> activationMap) {
+        Map<Word, Double> activationMapCopy = new TreeMap<>();
+        Iterator<CMNodeDO> nodeIterator = activationMap.keySet().iterator();
+        while (nodeIterator.hasNext()) {
+            CMNodeDO currentNode = nodeIterator.next();
+            activationMapCopy.put(currentNode.word, activationMap.get(currentNode));
+        }
+        this.activationHistory.add(activationMapCopy);
+    }
+
+    public void saveNodes(CMGraphDO graph) {
+        graph.nodeList.stream().forEach((node) -> {
+            this.addNodeIfNotExists(node);
+        });
+    }
+
+    private void addNodeIfNotExists(CMNodeDO nodeToAdd) {
+        boolean exists = false;
+        for (int i = 0; i < this.uniqueWordList.size(); i++) {
+            CMNodeDO currentNode = this.uniqueWordList.get(i);
+            if (currentNode.equals(nodeToAdd)) {
+                if (currentNode.nodeType != CMNodeType.Syntactic && nodeToAdd.nodeType == CMNodeType.Syntactic) {
+                    this.uniqueWordList.set(i, nodeToAdd);
+                }
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            this.uniqueWordList.add(nodeToAdd);
+        }
+    }
+
+    public void logSavedScores() {
+        try (FileWriter fwrt = new FileWriter(ActivationScoreLogger.OUTPUT_FILE_NAME); BufferedWriter bfwrt = new BufferedWriter(fwrt)) {
+            String header = "Word,Type";
+            for (int i = 0; i < activationHistory.size(); i++) {
+                header += ",Phrase " + (i + 1);
+            }
+            bfwrt.write(header);
+
+            for (CMNodeDO node : this.uniqueWordList) {
+                if (node.nodeType == CMNodeType.Syntactic) {
+                    String line = this.getLogLineForNode(node, "Syntactic");
+                    bfwrt.newLine();
+                    bfwrt.write(line);
+                }
+            }
+
+            for (CMNodeDO node : this.uniqueWordList) {
+                if (node.nodeType != CMNodeType.Syntactic) {
+                    String line = this.getLogLineForNode(node, "Semantic");
+                    bfwrt.newLine();
+                    bfwrt.write(line);
+                }
+            }
+        } catch (Exception e) {
+            Exceptions.printStackTrace(e);
+        }
+    }
+
+    private String getLogLineForNode(CMNodeDO node, String nodeType) {
+        String line = node.word.getLemma() + "," + nodeType;
+
+        for (Map<Word, Double> activationMap : this.activationHistory) {
+            if (!activationMap.containsKey(node.word)) {
+                line += ",0";
+            } else {
+                line += "," + activationMap.get(node.word);
+            }
+        }
+        return line;
+    }
+
 }
