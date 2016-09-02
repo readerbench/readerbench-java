674b516ded5e936866ad496d4cf1bddc01d2fc6c
gabi.gutu <gabi.gutu@gmail.com>
2016-09-02 17:36:32 +0300

    run.sh checks whether the necessary programs are installed
    Checks whether JAVA_HOME and M2_HOME exists
    Checks whether mvn command exists

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
-class=${1-webService.ReaderBenchServer}
-set -x
-mvn -Dexec.args="-Xss1g -Xmx4g -classpath %classpath $class" -Dexec.executable=java -Dexec.classpathScope=runtime -Dexec.longClasspath=true -Dfile.encoding=UTF-8 org.codehaus.mojo:exec-maven-plugin:1.5.0:exec
+if [ -z $JAVA_HOME ];
+then
+	echo "JAVA_HOME is not set!";
+else
+	if [ -z $M2_HOME ];
+	then
+		echo "M2_HOME is not set! Maven might not be installed on your computer!";
+	else
+		if [ -z $(which mvn) ]
+		then
+			echo "mvn is not installed";
+		else
+			class=${1-webService.ReaderBenchServer}
+			set -x
+			mvn -Dexec.args="-Xss1g -Xmx4g -classpath %classpath $class" -Dexec.executable=java -Dexec.classpathScope=runtime -Dexec.longClasspath=true -Dfile.encoding=UTF-8 org.codehaus.mojo:exec-maven-plugin:1.5.0:exec
+		fi
+	fi
+fi
