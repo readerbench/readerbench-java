80f9c79c174319c0c7d6ce64446f2a87dee72196
Stefan <stefan.ruseti@gmail.com>
2017-01-07 17:10:23 +0200

    #24 - Refactor Block/Sentence indices into same class

---
+++ src/main/java/services/complexity/surface/AvgWords.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 ReaderBench.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package services.complexity.surface;
+
+import data.AbstractDocument;
+import org.apache.commons.lang3.StringUtils;
+import services.complexity.AbstractComplexityIndex;
+import services.complexity.ComplexityIndicesEnum;
+import services.complexity.ComplexityIndex;
+import services.complexity.ComplexityIndices;
+import utils.IndexLevel;
+
+/**
+ *
+ * @author Stefan Ruseti
+ */
+public class AvgWords extends AbstractComplexityIndex {
+
+    public AvgWords(ComplexityIndicesEnum index, IndexLevel level) {
+        super(index, level);
+    }
+
+    @Override
+    public double compute(AbstractDocument d) {
+        return streamFunction.apply(d)
+                .mapToInt(b -> b.getWordOccurences().values().stream()
+                        .mapToInt(x -> x)
+                        .sum())
+                .average().orElse(ComplexityIndices.IDENTITY);
+    }
+    
+
+}
