21158034b0271f4673719e60c3808e6fd6111932
Gabi Gutu <gabi.gutu@gmail.com>
2017-01-13 17:31:13 +0200

    Fixed ignored words problem

--- src/main/java/webService/services/ConceptMap.java
+++ src/main/java/webService/services/ConceptMap.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import services.commons.Formatting;
 import services.discourse.keywordMining.KeywordModeling;
 import webService.result.ResultEdge;
@@ -51,12 +50,13 @@ public class ConceptMap {
      * @param noTopics
      * @return List of keywords and corresponding relevance scores for results
      */
-    public static ResultTopic getTopics(AbstractDocument queryDoc, double threshold, Set<String> ignoredWords, int noTopics) {
+    public static ResultTopic getTopics(AbstractDocument queryDoc, double threshold, Set<Word> ignoredWords, int noTopics) {
         List<AbstractDocument> queryDocs = new ArrayList();
         queryDocs.add(queryDoc);
         return getTopics(queryDocs, threshold, ignoredWords, noTopics);
     }
-    public static ResultTopic getTopics(List<? extends AbstractDocument> queryDocs, double threshold, Set<String> ignoredWords, int noTopics) {
+
+    public static ResultTopic getTopics(List<? extends AbstractDocument> queryDocs, double threshold, Set<Word> ignoredWords, int noTopics) {
 
         List<ResultNode> nodes = new ArrayList<>();
         List<ResultEdge> links = new ArrayList<>();
@@ -64,67 +64,41 @@ public class ConceptMap {
         List<Keyword> topics = new ArrayList();
         Map<Word, Double> topicScores = KeywordModeling.getCollectionTopics(queryDocs);
         for (Map.Entry<Word, Double> entry : topicScores.entrySet()) {
-            topics.add(new Keyword(entry.getKey(), entry.getValue()));
+            if (ignoredWords != null && !ignoredWords.contains(entry.getKey())) {
+                topics.add(new Keyword(entry.getKey(), entry.getValue()));
+            }
         }
         Collections.sort(topics);
-        
-        // TODO: remove this and add at line #84 visible concepts sublist extraction
+
         topics = KeywordModeling.getSublist(topics, noTopics, true, true);
 
-        // build connected graph
-        Map<Word, Boolean> visibleConcepts = new TreeMap<>();
         // build nodes
         SentimentGrid<Double> edges = new SentimentGrid<>(topics.size(), topics.size());
         Map<Word, Integer> nodeIndexes = new TreeMap<>();
-
-        for (Keyword t : topics) {
-            visibleConcepts.put(t.getWord(), false);
-        }
-
-        // determine similarities
-        for (Word w1 : visibleConcepts.keySet()) {
-            for (Word w2 : visibleConcepts.keySet()) {
-                double sim = SemanticCohesion.getAverageSemanticModelSimilarity(w1, w2);
-                if (!w1.equals(w2) && sim >= threshold) {
-                    visibleConcepts.put(w1, true);
-                    visibleConcepts.put(w2, true);
-                }
-            }
-        }
-
+        
         // determine optimal sizes
         double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
         for (Keyword t : topics) {
-            if (visibleConcepts.get(t.getWord()) && t.getRelevance() >= 0) {
+            if (t.getRelevance() >= 0) {
                 min = Math.min(min, Math.log(1 + t.getRelevance()));
                 max = Math.max(max, Math.log(1 + t.getRelevance()));
             }
         }
 
-        int i = 0, j;
+        int i = 0, j = 0;
         for (Keyword t : topics) {
-            if (visibleConcepts.get(t.getWord())) {
-                double nodeSize = 0;
-                if (max != min && t.getRelevance() >= 0) {
-                    nodeSize = (MIN_SIZE
-                            + (Math.log(1 + t.getRelevance()) - min) / (max - min) * (MAX_SIZE_TOPIC - MIN_SIZE));
-                } else {
-                    nodeSize = MIN_SIZE;
-                }
                 nodeIndexes.put(t.getWord(), i);
                 nodes.add(new ResultNode(i++, t.getWord().getLemma(), Formatting.formatNumber(t.getRelevance()), 1));
-            }
         }
 
         // determine similarities
         i = 0;
-        j = 0;
-        for (Word w1 : visibleConcepts.keySet()) {
-            edges.setIndex(w1.toString(), i++);
-            for (Word w2 : visibleConcepts.keySet()) {
-                edges.setIndex(w2.toString(), j++);
-                if (!w1.equals(w2) && visibleConcepts.get(w1) && visibleConcepts.get(w2)) {
-                    double sim = SemanticCohesion.getAverageSemanticModelSimilarity(w1, w2);
+        for (Keyword t1 : topics) {
+            edges.setIndex(t1.getWord().getLemma(), i++);
+            for (Keyword t2 : topics) {
+                edges.setIndex(t2.getWord().getLemma(), j++);
+                if (!t1.equals(t2)) {
+                    double sim = SemanticCohesion.getAverageSemanticModelSimilarity(t1.getWord(), t2.getWord());
                     if (sim >= threshold) {
                         double distance;
                         if (sim > .9) {
@@ -132,7 +106,7 @@ public class ConceptMap {
                         } else {
                             distance = (1f - sim) * 10;
                         }
-                        links.add(new ResultEdge("", nodeIndexes.get(w1), nodeIndexes.get(w2), Formatting.formatNumber(distance)));
+                        links.add(new ResultEdge("", nodeIndexes.get(t1.getWord()), nodeIndexes.get(t2.getWord()), Formatting.formatNumber(distance)));
                     }
                 }
             }
