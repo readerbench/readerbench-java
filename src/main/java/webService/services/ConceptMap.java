fc9f3d9ad33d789398f4e939b17103027b765f17
Gabi Gutu <gabi.gutu@gmail.com>
2017-02-17 16:55:19 +0200

    Advanced topic adjustments

--- src/main/java/webService/services/ConceptMap.java
+++ src/main/java/webService/services/ConceptMap.java
@@ -23,11 +23,11 @@ import data.discourse.SemanticCohesion;
 import data.sentiment.SentimentGrid;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import services.commons.Formatting;
 import services.complexity.wordComplexity.WordComplexity;
 import services.discourse.keywordMining.KeywordModeling;
 import services.semanticModels.LSA.LSA;
@@ -105,7 +105,7 @@ public class ConceptMap {
         int i = 0, j = 0;
         for (Keyword t : topics) {
             nodeIndexes.put(t.getWord(), i);
-            nodes.add(new ResultNode(i++, t.getWord().getLemma(), Formatting.formatNumber(t.getRelevance()), 1));
+            nodes.add(new ResultNode(i++, t.getWord().getLemma(), t.getRelevance(), 1));
         }
 
         // determine similarities
@@ -123,7 +123,7 @@ public class ConceptMap {
                         } else {
                             distance = (1f - sim) * 10;
                         }
-                        links.add(new ResultEdge("", nodeIndexes.get(t1.getWord()), nodeIndexes.get(t2.getWord()), Formatting.formatNumber(distance)));
+                        links.add(new ResultEdge("", nodeIndexes.get(t1.getWord()), nodeIndexes.get(t2.getWord()), distance));
                     }
                 }
             }
@@ -132,13 +132,13 @@ public class ConceptMap {
         return new ResultTopic(nodes, links);
     }
 
-    public static ResultTopicAdvanced getTopicsAdvanced(AbstractDocument queryDoc, double threshold, Set<Word> ignoredWords, int noTopics) {
+    public static ResultTopicAdvanced getTopicsAdvanced(AbstractDocument queryDoc, double threshold, Set<Word> ignoredWords) {
         List<AbstractDocument> queryDocs = new ArrayList();
         queryDocs.add(queryDoc);
-        return getTopicsAdvanced(queryDocs, threshold, ignoredWords, noTopics);
+        return getTopicsAdvanced(queryDocs, threshold, ignoredWords);
     }
 
-    public static ResultTopicAdvanced getTopicsAdvanced(List<? extends AbstractDocument> queryDocs, double threshold, Set<Word> ignoredWords, int noTopics) {
+    public static ResultTopicAdvanced getTopicsAdvanced(List<? extends AbstractDocument> queryDocs, double threshold, Set<Word> ignoredWords) {
 
         List<ResultNodeAdvanced> nodes = new ArrayList<>();
         List<ResultEdge> links = new ArrayList<>();
@@ -156,20 +156,8 @@ public class ConceptMap {
         }
         Collections.sort(topics);
 
-        topics = KeywordModeling.getSublist(topics, noTopics, true, true);
-
         // build nodes
-        SentimentGrid<Double> edges = new SentimentGrid<>(topics.size(), topics.size());
         Map<Word, Integer> nodeIndexes = new TreeMap<>();
-
-        // determine optimal sizes
-        double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
-        for (Keyword t : topics) {
-            if (t.getRelevance() >= 0) {
-                min = Math.min(min, Math.log(1 + t.getRelevance()));
-                max = Math.max(max, Math.log(1 + t.getRelevance()));
-            }
-        }
 
         int i = 0, j = 0;
         Lang lang = queryDocs.get(0).getLanguage();
@@ -179,45 +167,62 @@ public class ConceptMap {
         Map<Word, Double> mapIdf = lsa.getMapIdf();
         Map<Word, Integer> wordOcc = queryDocs.get(0).getWordOccurences();
         for (Keyword t : topics) {
-            ResultNodeAdvanced node = new ResultNodeAdvanced(i++, t.getWord().getText(), Formatting.formatNumber(t.getRelevance()), 1);
+            ResultNodeAdvanced node = new ResultNodeAdvanced(i, t.getWord().getText(), t.getRelevance(), 1);
             nodeIndexes.put(t.getWord(), i);
             node.setLemma(t.getWord().getLemma());
             node.setPos(t.getWord().getPOS());
             t.updateRelevance(queryDocs.get(0), t.getWord());
-            node.setTf(Formatting.formatNumber(t.getTermFrequency()));
-            node.setIdf(Formatting.formatNumber(mapIdf.get(t.getWord())));
-            
+            node.setNoOcc(wordOcc.get(t.getWord()));
+            node.setTf(t.getTermFrequency());
+            if (mapIdf.containsKey(t.getWord())) {
+                node.setIdf(mapIdf.get(t.getWord()));
+            } else {
+                node.setIdf(-1);
+            }
+
             // similarity scores between word and document using each semantic model
-            node.addSemanticSimilarity(SimilarityType.LSA.getAcronym(), Formatting.formatNumber(lsa.getSimilarity(t.getWord(), queryDocs.get(0))));
-            node.addSemanticSimilarity(SimilarityType.LDA.getAcronym(), Formatting.formatNumber(lda.getSimilarity(t.getWord(), queryDocs.get(0))));
-            if (word2Vec != null)
-                node.addSemanticSimilarity(SimilarityType.WORD2VEC.getAcronym(), Formatting.formatNumber(word2Vec.getSimilarity(t.getWord(), queryDocs.get(0))));
-            
+            node.addSemanticSimilarity(SimilarityType.LSA.getAcronym(), lsa.getSimilarity(t.getWord(), queryDocs.get(0)));
+            node.addSemanticSimilarity(SimilarityType.LDA.getAcronym(), lda.getSimilarity(t.getWord(), queryDocs.get(0)));
+            if (word2Vec != null) {
+                node.addSemanticSimilarity(SimilarityType.WORD2VEC.getAcronym(), word2Vec.getSimilarity(t.getWord(), queryDocs.get(0)));
+            }
+
             node.setAverageDistanceToHypernymTreeRoot(WordComplexity.getAverageDistanceToHypernymTreeRoot(t.getWord(), lang));
             node.setMaxDistanceToHypernymTreeRoot(WordComplexity.getMaxDistanceToHypernymTreeRoot(t.getWord(), lang));
             node.setPolysemyCount(WordComplexity.getPolysemyCount(t.getWord()));
             nodes.add(node);
+            i++;
         }
-
+        
         // determine similarities
         i = 0;
         for (Keyword t1 : topics) {
-            edges.setIndex(t1.getWord().getLemma(), i++);
             for (Keyword t2 : topics) {
-                edges.setIndex(t2.getWord().getLemma(), j++);
                 if (!t1.equals(t2)) {
                     double sim = SemanticCohesion.getAverageSemanticModelSimilarity(t1.getWord(), t2.getWord());
+                    System.out.println (sim);
                     if (sim >= threshold) {
-                        double distance;
-                        if (sim > .9) {
-                            distance = 1;
-                        } else {
-                            distance = (1f - sim) * 10;
-                        }
-                        links.add(new ResultEdge("", nodeIndexes.get(t1.getWord()), nodeIndexes.get(t2.getWord()), Formatting.formatNumber(distance)));
+                        links.add(new ResultEdge("", nodeIndexes.get(t1.getWord()), nodeIndexes.get(t2.getWord()), sim));
                     }
                 }
             }
+        }
+        Collections.sort(links);
+
+        // determine number of links and degree of nodes
+        Map<Integer, Integer> noLinks = new HashMap<>();
+        Map<Integer, Double> degree = new HashMap<>();
+        for (Integer index : nodeIndexes.values()) {
+            noLinks.put(index, 0);
+            degree.put(index, 0d);
+        }
+        for (ResultEdge link : links) {
+            noLinks.put(link.getSource(), noLinks.get(link.getSource()) + 1);
+            degree.put(link.getSource(), degree.get(link.getSource()) + link.getScore());
+        }
+        for (ResultNodeAdvanced node : nodes) {
+            node.setNoLinks(noLinks.get(node.getId()));
+            node.setDegree(degree.get(node.getId()));
         }
 
         return new ResultTopicAdvanced(nodes, links);
