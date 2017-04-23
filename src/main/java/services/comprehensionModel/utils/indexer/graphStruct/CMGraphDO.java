6ddf4b3eda878a8a87d8c542ec03411d05c5bc88
ionut.paraschiv <ionut.paraschiv@3angle.tech>
2017-04-23 16:25:35 +0300

    [HotFix] Do not restrict dictionary expansion using the no of words from current phrase

--- src/main/java/services/comprehensionModel/utils/indexer/graphStruct/CMGraphDO.java
+++ src/main/java/services/comprehensionModel/utils/indexer/graphStruct/CMGraphDO.java
@@ -242,8 +242,9 @@ public class CMGraphDO {
         // sort the potential inferred edge list
         Collections.sort(potentialInferredNodeList, (n1, n2) -> (-1) * new Double(n1.getActivationScore()).compareTo(n2.getActivationScore()));
 
-        // keep only the first `maxInferredExpand` possible inf
-        int maxInferredExpand = Math.min(maxDictionaryExpansion, syntacticGraph.getNodeList().size());
+        // expand up to maxDictionaryExpansion dictionary words
+        int maxInferredExpand = maxDictionaryExpansion;
+        
         List<CMNodeDO> inferredNodeList = potentialInferredNodeList.subList(0, Math.min(maxInferredExpand, potentialInferredNodeList.size()));
 
         inferredNodeList.stream().forEach(inferredNode -> {
