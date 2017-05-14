c53965a82275914febbc9a0c4983a26261004d31
Dorinela Sirbu <dorinela.92@gmail.com>
2017-05-14 15:45:39 +0300

    French reviews

--- src/main/java/runtime/cscl/frenchdata/FrenchData.java
+++ src/main/java/runtime/cscl/frenchdata/FrenchData.java
@@ -3,6 +3,11 @@ package runtime.cscl.frenchdata;
 import data.AbstractDocument;
 import data.AbstractDocumentTemplate;
 import data.Lang;
+import data.cscl.CSCLCriteria;
+import data.cscl.CSCLIndices;
+import data.cscl.Conversation;
+import data.cscl.Participant;
+import data.discourse.Keyword;
 import data.document.Document;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -14,8 +19,13 @@ import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
+import org.openide.util.Exceptions;
 import org.slf4j.LoggerFactory;
+import services.commons.Formatting;
+import services.commons.VectorAlgebra;
 import services.complexity.ComplexityIndex;
+import services.complexity.ComplexityIndices;
+import services.discourse.keywordMining.KeywordModeling;
 import services.semanticModels.ISemanticModel;
 import services.semanticModels.SimilarityType;
 
@@ -47,10 +57,21 @@ public class FrenchData {
             }
             sheet = workbook.getSheetAt(0);
 
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < docs.size(); i++) {
+                System.out.println("Start doc " + i);
                 AbstractDocument abstractDocument = frenchData.loadDocument(docs.get(i).getContent());
                 abstractDocument.computeAll(true);
                 Map<ComplexityIndex, Double> complexityIndex = abstractDocument.getComplexityIndices();
+                Map<String, Double> complexityIndexValue = new HashMap<>();
+                for (Map.Entry<ComplexityIndex, Double> entry : complexityIndex.entrySet()) {
+                    complexityIndexValue.put(entry.getKey().getAcronym(), entry.getValue());
+                }
+
+                Map<String, Double> sortMap = new LinkedHashMap<>();
+
+                complexityIndexValue.entrySet().stream()
+                        .sorted(Map.Entry.<String, Double>comparingByKey())
+                        .forEachOrdered(x -> sortMap.put(x.getKey(), x.getValue()));
 
                 int rows = sheet.getPhysicalNumberOfRows();
                 Row row = sheet.createRow(rows);
@@ -62,8 +83,9 @@ public class FrenchData {
                 cell.setCellValue(docs.get(i).getScore());
 
                 int start = 2;
-                for (Map.Entry<ComplexityIndex, Double> entry : complexityIndex.entrySet())
+                for (Map.Entry<String, Double> entry : sortMap.entrySet())
                 {
+                    //System.out.print(entry.getKey() + ",");
                     cell = row.createCell(start);
                     cell.setCellValue(entry.getValue());
                     start++;
@@ -78,77 +100,15 @@ public class FrenchData {
             System.out.println("Exception: " + e.getMessage());
         }
 
-//        System.out.println("----------- Start --------");
-//        for (int i = 0; i < 1; i++) {
-//            AbstractDocument abstractDocument = frenchData.loadDocument(docs.get(i).getContent());
-//            abstractDocument.computeAll(true);
-//            Map<ComplexityIndex, Double> complexityIndex = abstractDocument.getComplexityIndices();
-//            frenchData.writeFinalDataList(docs.get(i), complexityIndex);
-//        }
-//        System.out.println("----------- Finish --------");
     }
 
-    public void writeFinalDataList(Doc doc, Map<ComplexityIndex, Double> complexityIndex) {
-        XSSFWorkbook workbook = null;
-        try {
-            XSSFSheet sheet;
-            if (workbook == null) {
-                workbook = new XSSFWorkbook();
-                sheet = workbook.createSheet();
-            }
-            sheet = workbook.getSheetAt(0);
-            //for(FinalMessage finalMessage : finalMessages) {
-                int rows = sheet.getPhysicalNumberOfRows();
-                Row row = sheet.createRow(rows);
-
-                Cell cell = row.createCell(0);
-                cell.setCellValue(doc.getGame());
-
-                cell = row.createCell(1);
-                cell.setCellValue(doc.getScore());
-
-                int start = 2;
-                for (Map.Entry<ComplexityIndex, Double> entry : complexityIndex.entrySet())
-                {
-                    cell = row.createCell(start);
-                    cell.setCellValue(entry.getValue());
-                    start++;
-                }
-            //}
-            FileOutputStream outputStream = new FileOutputStream("D:\\Facultate\\MASTER\\french_reviews\\french_reviews.xlsx");
-            workbook.write(outputStream);
-            System.out.println("Finish to write all data in file !!!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Exception: " + e.getMessage());
-        }
-    }
-
-    public void writeFile(List<Doc> docs) throws IOException {
-//        File fout = new File("D:\\Facultate\\MASTER\\french_reviews\\french_reviews.txt");
-//        FileOutputStream fos = new FileOutputStream(fout);
-//
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-//
-//        for (Doc doc : docs) {
-//            bw.write(doc.getContent().trim());
-//        }
-//        bw.close();
-
-        PrintWriter pw = new PrintWriter(new FileWriter("D:\\Facultate\\MASTER\\french_reviews\\french_reviews.txt"));
-        for (Doc doc : docs) {
-            pw.write(doc.getContent().trim());
-        }
-        pw.close();
-
-    }
 
     public AbstractDocument loadDocument(String text) {
 
         Map<SimilarityType, String> modelPaths = new EnumMap<>(SimilarityType.class);
         modelPaths.put(SimilarityType.LSA, LSA_PATH);
         modelPaths.put(SimilarityType.LDA, LDA_PATH);
-        modelPaths.put(SimilarityType.WORD2VEC, WORD2VEC_PATH);
+        //modelPaths.put(SimilarityType.WORD2VEC, WORD2VEC_PATH);
         List<ISemanticModel> models = SimilarityType.loadVectorModels(modelPaths, Lang.fr);
 
         AbstractDocumentTemplate contents = AbstractDocumentTemplate.getDocumentModel(text);
