0342af5546b35749f44971eae2efd2a376c49a55
valentin_cioaca <admin_licenta@172.19.3.150>
2017-06-29 19:06:26 +0300

    new entropy based formula for regularity

--- src/main/java/services/commons/VectorAlgebra.java
+++ src/main/java/services/commons/VectorAlgebra.java
@@ -481,6 +481,17 @@ public class VectorAlgebra {
         }
         return Math.pow(sum, 1d / p);
     }
+    
+    public static double peakOnChatFrame(double[] C) {
+        double E_c = VectorAlgebra.entropy(C);
+        double max = 0;
+        
+        for (double v : C) {
+            if (v > max)
+                max = v;
+        }
+        return (Math.log(C.length) - E_c) * max;
+    }
 
     public static void main(String[] args) {
         double[] v = {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
