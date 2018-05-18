442edd0660818d3887e3f3e0ba3eee36ff16b477
stefan <stefan@172.19.3.231>
2018-05-18 17:17:57 +0300

    minor fix

--- textual-complexity/src/main/java/com/readerbench/textualcomplexity/wordLists/WordValences.java
+++ textual-complexity/src/main/java/com/readerbench/textualcomplexity/wordLists/WordValences.java
@@ -29,7 +29,7 @@ public class WordValences {
     private static final Map<Lang, List<String>> valencesForLang  = new EnumMap<>(Lang.class);
     
     private static void initLang(Lang lang) {
-        String path = "resources/new_config/wordlists-" + lang.name() + "/valences_" + lang.name() + ".csv";
+        String path = "../resources/new_config/wordlists-" + lang.name() + "/valences_" + lang.name() + ".csv";
         map.put(lang, new HashMap<>());
         try (BufferedReader in = new BufferedReader(new FileReader(path))) {
             String header = in.readLine();
@@ -47,7 +47,7 @@ public class WordValences {
                 String[] values = line.split(";");
                 Map<String, Double> wordValues = new HashMap<>();
                 for (int i = 1; i < values.length; i++) {
-                    wordValues.put(valences.get(i), Double.parseDouble(values[i]));
+                    wordValues.put(splitHeader[i], Double.parseDouble(values[i]));
                 }
                 map.get(lang).put(values[0], wordValues);
             }
@@ -72,5 +72,9 @@ public class WordValences {
             initLang(lang);
         }
         return valencesForLang.get(lang);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(getValenceForWord(new Word("hate", null, null, null, null, Lang.en), "Valence_ANEW"));
     }
 }
