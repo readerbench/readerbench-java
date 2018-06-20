33a97f64cdeb43cdcc4860458a291794f0815aff
stefan <stefan@172.19.3.231>
2018-06-20 15:35:26 +0300

    comprehension model - all methods that use Stanford coreferences were commented

--- comprehension-model/src/main/java/com/readerbench/comprehensionmodel/utils/distanceStrategies/utils/CMCoref.java
+++ comprehension-model/src/main/java/com/readerbench/comprehensionmodel/utils/distanceStrategies/utils/CMCoref.java
@@ -15,27 +15,25 @@
  */
 package com.readerbench.comprehensionmodel.utils.distanceStrategies.utils;
 
-import edu.stanford.nlp.ling.CoreLabel;
-
 public class CMCoref {
 
-    private CoreLabel token;
-    private CoreLabel referencedToken;
+//    private CoreLabel token;
+//    private CoreLabel referencedToken;
 
-    public CMCoref(CoreLabel token, CoreLabel referencedToken) {
-        this.token = token;
-        this.referencedToken = referencedToken;
-    }
-
-    public CoreLabel getToken() {
-        return token;
-    }
-
-    public CoreLabel getReferencedToken() {
-        return referencedToken;
-    }
-
-    public int getSentenceIndex() {
-        return this.token.sentIndex();
-    }
+//    public CMCoref(CoreLabel token, CoreLabel referencedToken) {
+//        this.token = token;
+//        this.referencedToken = referencedToken;
+//    }
+//
+//    public CoreLabel getToken() {
+//        return token;
+//    }
+//
+//    public CoreLabel getReferencedToken() {
+//        return referencedToken;
+//    }
+//
+//    public int getSentenceIndex() {
+//        return this.token.sentIndex();
+//    }
 }
